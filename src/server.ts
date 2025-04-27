19296f1e1d8c435140749cbb4eb6be4af7161212
Tabs Macedo <tabata.macedo1897@gmail.com>
2025-04-26 23:21:09 -0300

    feature/server-connectDB

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,28 @@
 import dotenv from 'dotenv';
-dotenv.config();  
-// chamar as rotas e o cors
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import path from 'path';
+import Database from './config/connection';
+
+const envPath = path.resolve(process.cwd(), `.env.${process.env.NODE_ENV}`)
+dotenv.config({path: envPath})
+
+class Server {
+  public express: express.Application;
+
+  constructor() {
+    this.express = express();
+    this.middlewares();
+    const db = new Database();
+    db.mongooseConnect();
+  }
+
+  private middlewares() {
+    this.express.use(express.json());
+    this.express.use(cors());
+    this.express.use(bodyParser.urlencoded({ extended: false }));
+  }
+}
+
+export default new Server().express;
