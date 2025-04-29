f593793377ca38c0570ba238dda48d2cf1370be9
Tabs Macedo <tabata.macedo1897@gmail.com>
2025-04-28 22:17:34 -0300

    feat: adição do routes e model

---
+++ src/schemas/UserSchema.ts
@@ -0,0 +1,23 @@
+import { model, Schema } from 'mongoose';
+import { IUser } from '../models/interface/IUser';
+
+const User = new Schema({
+  name: {
+    type: String
+  },
+  email: {
+    type: String
+  },
+  password: {
+    type: String
+  },
+  phone: {
+    type: String
+  },
+  permission: {
+  }
+}, {
+  timestamps: true
+});
+
+export default model<IUser>('User', User);
--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import path from 'path';
 import Database from './config/connection';
-import feedbackRouter from './routes/UserRoutes'
+import initRoutes from './routes/initRoutes';
 
 
 const envPath = path.resolve(process.cwd(), `.env.${process.env.NODE_ENV}`)
@@ -24,7 +24,11 @@ class Server {
     this.express.use(express.json());
     this.express.use(cors());
     this.express.use(bodyParser.urlencoded({ extended: false }));
-    this.express.use('/api', feedbackRouter); 
+    this.routes()
+  }
+  private routes(){
+    const prefix = '/api'
+    this.express.use(prefix, initRoutes)
   }
 }
 
